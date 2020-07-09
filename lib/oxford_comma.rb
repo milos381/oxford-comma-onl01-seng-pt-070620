ffab4dd90765bb4b22c5b10f3fae17de91ce17f9
Milos Popovic <milos_popovic@outlook.com>
2020-07-09 02:59:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,7 @@ def oxford_comma(array)
     array.join(" and ")
   else
     #array[0...-2].join(", ") << array.last(2).join(", and ")
-    array[0..-2].join(', ') + ", and " + array[-1]
+    array[0..-2].join(', ') << ", and " + array[-1]
   end
 
 end
