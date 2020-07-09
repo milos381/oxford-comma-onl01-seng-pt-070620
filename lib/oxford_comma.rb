50bf734e6572e11412e2eeaf48ae7ae2ad287913
Milos Popovic <milos_popovic@outlook.com>
2020-07-09 03:01:03 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
 
+  if array.length < 2
+    array.join(", ")
+
+  elsif array.length < 3
+    array.join(" and ")
+  else
+    #array[0...-2].join(", ") << array.last(2).join(", and ")
+    array[0..-2].join(', ') << ", and " + array[-1]
+  end
+
 end
