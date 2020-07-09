102ae2d036e67ed313b1768091fc8db804ba22bf
Milos Popovic <milos_popovic@outlook.com>
2020-07-09 02:47:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
 
+  if array.length < 2
+    array.join(", ")
+
+  elsif array.length < 3
+    array.join(" and ")
+  else
+    array[0..-1].join(", ") << array.last(2).join(", and ")
+  end
+
 end
